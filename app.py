dc786f3159fd803c5b0d90b4a2f32653d7698bfa
ClaytonCSilva <clayton.carllos08@gmail.com>
2025-03-30 14:13:36 -0300

    Subindo arquivo PetShop

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ from flask import flash
 import pytz
 
 
-
-
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Necessário para usar flash()
 
@@ -31,7 +29,7 @@ app.config['MAIL_DEFAULT_SENDER'] = 'clayton.carllos08@gmail.com'  # Remetente p
 mail = Mail(app)  # Inicializa o Flask-Mail
 
 # Configuração do banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:PlbhfPcHjqHJZUplwYanSSAWGFDpoyum@shinkansen.proxy.rlwy.net:25719/railway'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kkpxsqSiItqiOEyVjEnwrlWiQYapDbHE@shortline.proxy.rlwy.net:55238/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
